d390d4ee700d32e631c48762985b03fa13caef27
SightFad <lephan1210@gmail.com>
2024-11-13 18:04:08 +0700

    c

--- 23DH112541_MyStore/Controllers/AccountController.cs
+++ 23DH112541_MyStore/Controllers/AccountController.cs
@@ -3,20 +3,61 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Runtime.Remoting.Messaging;
 using System.Web.Security;
 using System.Web.UI.WebControls;
-using _23DH112541_MyStore.Models;
 using _23DH112541_MyStore.Models.ViewModel;
+using _23DH112541_MyStore.Models;
 
-namespace _23DH112541_MyStore.Controllers
+
+namespace MaSV_MyStore.Controllers
 {
     public class AccountController : Controller
     {
         private MyStoreEntities db = new MyStoreEntities();
-        // GET: Account
-        public ActionResult Index()
+        // GET: Account/Register
+        public ActionResult Register()
         {
             return View();
+        }
+        // POST: Account/Register
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Register(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                //kiểm tra xem tên đăng nhập đã tồn tại chưa
+                var existingUser = db.Users.SingleOrDefault(u => u.Username == model.Username);
+                if (existingUser != null)
+                {
+                    ModelState.AddModelError("Username", "Tên đăng nhập này đã tồn tại!");
+                    return View(model);
+                }
+                var user = new User
+                {
+                    Username = model.Username,
+                    Password = model.Password, // Lưu ý: Nên mã hóa mật khẩu trước khi lưu
+                    UserRole = "Customer"
+                };
+
+                db.Users.Add(user);
+                // và tạo bản ghi thông tin khách hàng trong bảng Customer
+                var customer = new Customer
+                {
+                    CustomerName = model.CustomerName,
+                    CustomerEmail = model.CustomerEmail,
+                    CustomerPhone = model.CustomerPhone,
+                    CustomerAddress = model.CustomerAddress,
+                    Username = model.Username,
+                };
+
+                db.Customers.Add(customer);
+                db.SaveChanges();
+                //Lưu thông tin tài khoản và thông tin khách hàng vào CSDL db.SaveChanges();
+                return RedirectToAction("Index", "Home");
+            }
+            return View(model);
         }
 
         // GET: Account/Login
@@ -24,7 +65,6 @@ namespace _23DH112541_MyStore.Controllers
         {
             return View();
         }
-
         // POST: Account/Login
         [HttpPost]
         [ValidateAntiForgeryToken]
@@ -35,15 +75,15 @@ namespace _23DH112541_MyStore.Controllers
                 var user = db.Users.SingleOrDefault(u => u.Username == model.Username
                 && u.Password == model.Password
                 && u.UserRole == "Customer");
-                if(user != null)
+                if (user != null)
                 {
-                    // Lưu trạng thái đăng nhập vào session 
+                    // Lưu trạng thái đăng nhập vào session
                     Session["Username"] = user.Username;
                     Session["UserRole"] = user.UserRole;
 
+
                     //lưu thông tin xác thực người dùng vào cookie
                     FormsAuthentication.SetAuthCookie(user.Username, false);
-
                     return RedirectToAction("Index", "Home");
                 }
                 else
@@ -54,5 +94,132 @@ namespace _23DH112541_MyStore.Controllers
 
             return View(model);
         }
+
+        // GET: Account/Logout
+        public ActionResult Logout()
+        {
+            Session.Clear();
+            return RedirectToAction("Login", "Account");
+        }
+
+        // GET: Account/Profile
+        [Authorize]
+        public ActionResult ProfileInfo()
+        {
+            var user = db.Users.SingleOrDefault(u => u.Username == User.Identity.Name);
+            if (user == null)
+            {
+                return RedirectToAction("Login", "Account");
+            }
+
+            var customer = db.Customers.SingleOrDefault(c => c.Username == user.Username);
+            if (customer == null)
+            {
+                return RedirectToAction("Index", "Home");
+            }
+            var model = new RegisterVM
+            {
+                Username = user.Username,
+                CustomerName = customer.CustomerName,
+                CustomerPhone = customer.CustomerPhone,
+                CustomerEmail = customer.CustomerEmail,
+                CustomerAddress = customer.CustomerAddress
+            };
+
+            return View(model);
+        }
+
+        // POST: Account/Profile
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult ProfileInfo(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.SingleOrDefault(u => u.Username == User.Identity.Name);
+                if (user == null)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                var customer = db.Customers.SingleOrDefault(c => c.Username == user.Username);
+                if (customer == null)
+                {
+                    return RedirectToAction("Index", "Home");
+                }
+
+                customer.CustomerName = model.CustomerName;
+                customer.CustomerPhone = model.CustomerPhone;
+                customer.CustomerEmail = model.CustomerEmail;
+                customer.CustomerAddress = model.CustomerAddress;
+                db.SaveChanges();
+                return RedirectToAction("Profile");
+            }
+            return View(model);
+        }
+
+        // GET: Account/Change Password [Authorize]
+        public ActionResult ChangePassword()
+        {
+            return View();
+        }
+
+        // POST: Account/Change Password
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize]
+        public ActionResult ChangePassword(RegisterVM model)
+        {
+            if (ModelState.IsValid)
+            {
+                var user = db.Users.SingleOrDefault(u => u.Username == User.Identity.Name);
+                if (user == null)
+                {
+                    return RedirectToAction("Login", "Account");
+                }
+                user.Password = model.Password; // Lưu ý: Nên mã hóa mật khẩu trước khi lưu db.SaveChanges();
+                return RedirectToAction("ProfileInfo");
+            }
+            return View(model);
+        }
+
+        // GET: Account/UpdateAccount/5
+        public ActionResult UpdateAccount(int id)
+        {
+            return View();
+        }
+        // POST: Account/UpdateAccount/5
+        [HttpPost]
+        public ActionResult UpdateAccount(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update Logic here
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+        // GET: Account/Change Password/5
+        public ActionResult ChangePassword(int id)
+        {
+            return View();
+        }
+        // POST: Account/Change Password/5
+        [HttpPost]
+        public ActionResult ChangePassword(int id, FormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
     }
 }
+
